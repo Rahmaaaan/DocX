121555abeadb559b7a9af60f6357efb3eba6396a
Aqueel ur Rahman Khan <119236505+Rahmaaaan@users.noreply.github.com>
2023-12-22 20:00:59 +0530

    Update index.ts

--- src/lib/stripe/index.ts
+++ src/lib/stripe/index.ts
@@ -2,7 +2,7 @@ import Stripe from 'stripe';
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY ?? '', {
   apiVersion: '2023-10-16',
   appInfo: {
-    name: 'Webprodigies Cypress',
+    name: 'DocX',
     version: '0.1.0',
   },
 });
